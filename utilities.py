c86d0fe87ac69e7ce2b27d2bfc8cd241932efc24
ASUS <ASUS@DESKTOP-RGPOVRB>
2022-11-25 10:57:22 +0330

    Fcommit

--- utilities.py
+++ utilities.py
@@ -31,6 +31,49 @@ def calc_mu(data):
     return mu
 
 
+#def calc_sigma(data):
+#    sigma = np.cov(data.iloc[:, :-1], rowvar=False)
+#   return sigma
+
+
 def calc_sigma(data):
-    sigma = np.cov(data.iloc[:, :-1], rowvar=False)
-    return sigma
+    x=data[['X1', 'X2']].values
+    y=data[['y']].values
+    mu=calc_mu(data)
+    cov = np.zeros(shape=(x.shape[1], x.shape[1]))
+    for i in range(x.shape[0]):
+        if y[i]==0:
+            x_mu=x[i]-mu[0]
+        else:
+            x_mu=x[i]-mu[1]
+            
+        cov +=(x_mu).reshape(x.shape[1],1).dot((x_mu).reshape(1,x.shape[1]))
+    cov = cov/x.shape[0]
+    return cov
+
+def Prediction(data, Mu, Sigma, Nclass):
+    X=data[['X1','X2']].values
+    y=data[['y']].values
+    Probabilities = []
+    p=calc_phi(y)
+    for i in range(Nclass):
+        Phi = ((p[i])**i * (1 - p[i])**(1 - i))
+        mu = Mu[i, :]
+        invSigma = np.linalg.pinv(Sigma)
+        Probability = np.log(Phi) +(-0.5 * np.sum((X-mu).dot(invSigma)*(X-mu), axis=1))
+        Probabilities.append(Probability)
+
+    Classes = np.argmax(Probabilities, axis=0)
+    return Classes, Probabilities
+
+
+def calc_accuracy(y, yh):
+    m_sample = len(y)
+    correct = 0
+    for i in range(m_sample):
+        if yh[i] == y[i]:
+            correct += 1
+    acc = correct / m_sample
+    return acc
+
+    
